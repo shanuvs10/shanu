2e8fd0cf8eb17c9cd404b4706a7f13a22589c9f5
shanuvs10 <103172844+shanuvs10@users.noreply.github.com>
2022-04-07 15:17:47 +0530

    Update composit.java

--- composit.java
+++ composit.java
@@ -1,9 +1,40 @@
 import java.util.*;
 class composit
 {
+    int n,m;
     void input()
     {
         Scanner sc =new Scanner(System.in);
-        
+        n=sc.nextInt();
+        m=sc.nextInt();
+    }
+    int comp(int q)
+    {
+        int a=q;
+        int c=0;
+        for(int i=1;i<=a;a/=10)
+        {
+            if(a%i==0)
+            c++;
+        }
+        if(c!=1||c!=2)
+        q=1;
+        return q; 
+    }
+    void magic()
+    {
+        int a=0;
+        while(m<=n)
+        {
+            int q=m;
+
+             while(q>0)
+             {
+               a=a+(q%10);
+               a*=10;
+               q/=10;
+            }
+            m++;
+        }
     }
 }
