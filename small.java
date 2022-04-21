a8c698313fe51cea4a7de4237dbc2eaf7d9bddd1
shanuvs10 <103172844+shanuvs10@users.noreply.github.com>
2022-04-21 14:56:04 +0530

    Update small.java

--- small.java
+++ small.java
@@ -3,6 +3,7 @@ import java.util.*;
 class small {
     int n, m, cout, a;
 
+
     void input() {
         Scanner sc = new Scanner(System.in);
         m = sc.nextInt();
@@ -10,12 +11,18 @@ class small {
     }
 
     void calcu()
-    // tutyu
     {
-        a = m - n;
-        for (int i = a; i > 0; i /= 10) {
-            cout++;
+        
+    }
+    int sum(int n){
+        int sum=0;
+        int d=0;
+        while (n>0) {
+            d=n%10;
+            sum+=d;
+            n/=10;
         }
+        return sum;
     }
 
     void disply() {
@@ -26,7 +33,15 @@ class small {
     public static void main(String[] args) {
         small obj = new small();
         obj.input();
-        obj.calcu();
+        int q = obj.m;
+        int w =obj.n;
+        if (q<1000||q>10000||w<1||w>99) {
+            System.out.println("Invalid Input");
+            
+        }
+        else{
+           for (int i= q ;obj.sum(i)!=w ; i++); 
+        }
         obj.disply();
     }
 
