7170b7b787d04131a0c4dbf8353786be796d95f6
shanuvs10 <103172844+shanuvs10@users.noreply.github.com>
2022-04-21 15:49:48 +0530

    Update composit.java

--- composit.java
+++ composit.java
@@ -1,67 +1,64 @@
 import java.util.*;
-class composit
-{
-    int n,m,c=0;
-    void input()
-    {
-        Scanner sc =new Scanner(System.in);
+
+class composit {
+    int n, m, c = 0;
+
+    void input() {
+        Scanner sc = new Scanner(System.in);
         System.out.println("Enter m");
-        m=sc.nextInt();
+        m = sc.nextInt();// Geting m from user
         System.out.println("Enter n");
-        n=sc.nextInt();
+        n = sc.nextInt();// Geting n from user
     }
-    boolean comp(boolean co)
+
+    boolean comp(boolean co) // method for finding composit no
     {
-        for(int i=m ;i<=n;i++)
-        {
-            co =false;
-            for (int j = 2; j <i; j++) {
-                if (i%j==0) {
-                    co =true;
+        for (int i = m; i <= n; i++) {
+            co = false;
+            for (int j = 2; j < i; j++) {
+                if (i % j == 0) {
+                    co = true;
                     break;
                 }
             }
         }
         return co;
-        
+
     }
-    void magic()
-    {
+
+    void magic() {
         int num;
-        while(m<=n)
-        {num=m;
-            while(num>9)
-            {
-                int s=0;
-                while(num!=0)
-                {
-                    int d =num%10;
-                    num/=10;
-                    s+=d;
+        while (m <= n)// finding magin no
+        {
+            num = m;
+            while (num > 9) {
+                int s = 0;
+                while (num != 0) {
+                    int d = num % 10;
+                    num /= 10;
+                    s += d;
                 }
-                num=s;
+                num = s;
             }
             m++;
-            if(num==1)
-            {
+            if (num == 1) {
                 c++;
-                System.out.print(m+"  ");
+                System.out.print(m + "  ");
             }
         }
         System.out.println("");
-        System.out.println("Frequency of the composite integer is : "+c);
+        System.out.println("Frequency of the composite integer is : " + c);
 
     }
+
     public static void main(String[] args) {
-        composit obj =new composit();
+        composit obj = new composit();
         obj.input();
         if (obj.comp(true)) {
-            obj.magic();  
-        }
-        else
-        {
+            obj.magic();
+        } else {
             System.out.println("INVALID INPUT");
         }
-        
+
     }
 }
