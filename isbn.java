befe3ab69045bf88498ca5fc67ef4bb10b0d9cab
shanuvs10 <103172844+shanuvs10@users.noreply.github.com>
2022-04-21 15:47:22 +0530

    Update isbn.java

--- isbn.java
+++ isbn.java
@@ -8,36 +8,36 @@ class isbn {
     void input() {
         Scanner sc = new Scanner(System.in);
         System.out.println("Enter the code");
-        a = sc.nextLine();
+        a = sc.nextLine();// Geting value from user
     }
 
     void calc() {
         if (a.length() == 10) {
-            for (int i = 0; i < a.length(); i++) {
+            for (int i = 0; i < a.length(); i++) {// changing
                 if (a.charAt(i) == 'x' || a.charAt(i) == 'X')
                     n = 10;
                 else
                     n = Integer.parseInt(Character.toString(a.charAt(i)));
-                sum += (n * (a.length() - i));
+                sum += (n * (a.length() - i));// calculation
             }
 
         }
 
     }
 
-    void print() {
-        if(a.length()!=10)
-        System.out.println("INVALID INPUT");
-        else{
-        System.out.println("sum " + sum);
-          if (sum % 11 == 0)
-            System.out.println("leaves no remainder -valid isbn code ");
-          else
+    void print() {// printing method
+        if (a.length() != 10)
             System.out.println("INVALID INPUT");
+        else {
+            System.out.println("sum " + sum);
+            if (sum % 11 == 0)
+                System.out.println("leaves no remainder -valid isbn code ");
+            else
+                System.out.println("INVALID INPUT");
         }
     }
 
-    public static void main(String[] args) {
+    public static void main(String[] args) {// main method
         isbn obj = new isbn();
         obj.input();
         obj.calc();
