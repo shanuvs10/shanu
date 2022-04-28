8a140b92681b9743c780d020c2ba56297785b1c2
Labrec123 <103172844+shanuvs10@users.noreply.github.com>
2022-04-28 15:07:33 +0530

    Update primecircular.java

--- primecircular.java
+++ primecircular.java
@@ -1,25 +1,42 @@
 import java.util.*;
 public class primecircular {
     String s;
+    int a;
     void input ()
     {
         Scanner sc = new Scanner(System.in);
         s=sc.nextLine();
+        a=sc.nextInt();
     }
     void calc()
     {
+        primecircular obj2 =new primecircular();
+        for (int i=0; i<=a.length(); i++); i++>)
+        {
+
+        }
+        if (obj2.Prime(a)) {
+            System.out.println("true");
+        }
 
     }
-    boolean isPrime(int n)
+    boolean Prime(int n)
     {
         boolean q= false;
-        for (int i=n; i>0 ; i--) {
-            if (i%==0) {
+        int co=0;
+        for (int i=1; i<=n ; i++) {
+            if (n%i==0) {
                 co++;
             }
         }
         if (co==2) {
             q=true;
         }
+        return q;
+    }
+    public static void main(String[] args) {
+        primecircular obj =new primecircular();
+        obj.input();
+        obj.calc();
     }
 }
