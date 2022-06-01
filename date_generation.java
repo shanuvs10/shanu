f31b9e445c4f72e57780a0be9baad181ae948fe5
shanuvs10 <103172844+shanuvs10@users.noreply.github.com>
2022-06-01 21:37:26 +0530

    Update date_generation.java

--- date_generation.java
+++ date_generation.java
@@ -1,4 +1,16 @@
 public class date_generation {
     
-    
+    boolean leap_year(int year)
+    {
+        boolean q=false;
+        if (year % 4==0) {
+            q=true;
+        }
+        
+        return q;
+        
+    }
+    public static void main(String[] args) {
+        date_generation
+    }
 }
