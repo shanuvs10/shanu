662f79cdd61d81c385ef1b976fd1f95c137d25ec
shanuvs10 <103172844+shanuvs10@users.noreply.github.com>
2022-07-04 20:02:43 +0530

    Update prime_adem_no.java

--- prime_adem_no.java
+++ prime_adem_no.java
@@ -6,54 +6,60 @@ public class prime_adem_no {
     boolean p;
     void input(){
         Scanner sc = new Scanner(System.in);
-        System.out.println("Enter INPUT n : ");
-        n = sc.nextInt();// Geting n from user
         System.out.println("Enter INPUT m : ");
-        m = sc.nextInt();// Geting m from user
+        m = sc.nextInt();// Geting n from user
+        System.out.println("Enter INPUT n : ");
+        n = sc.nextInt();// Geting m from user
     }
-    boolean primeno(int m){
-        boolean q=false;
+    boolean primeno(int z){
         int c=0;
-        for(int i=1;i<= m; i++){
-            if (m%i==0) {
+        for(int i=1;i<= z; i++){
+            if (z%i==0) {
                 c++;
             }
         }
-        if (c==2) {
-            q=true;
-        }
-        return q;
+        return c==2;
     }
-    int reverse(int n){
+    int reverse(int x){
         int t,s=0;
-        for(;n>0;n/=10){
-            t=n%10;
+        for(;x>0;x/=10){
+            t=x%10;
             s=(s*10)+t;
             
-            System.out.println(s);
         }
        
        return s;
     }
     boolean adam_no(int q){
-        int n=q;int rn =reverse(n);
-        
-        return p;
+        int sqq= q*q;
+        int revq= reverse(q);
+        int sqrevq=revq*revq;
+        int rev=reverse(sqq);
+        return rev == sqrevq;
     }
     void calc(){
+        if (m >= n) {
+            System.out.println("INVALID INPUT");
+            return;
+        }
         System.out.println("the integer are");
-        for (int i = m; i < n; i++) {
-            if (primeno(i)&&adam_no(i)) {
-                System.out.print(i+"\t");
+        while (m<=n) {
+            if (primeno(m)&&adam_no(m)) {
+                System.out.print(m+" ");
                 c++;
             }
+            m++;
+        }
+        System.out.println("");
+        if (c == 0) {
+            System.out.println("NIL");
         }
         System.out.println("Frequency of prime_adem integer is : "+c);
     }
     public static void main(String[] args) {
         prime_adem_no obj = new prime_adem_no(); 
-        //obj.input();
-        System.out.println(obj.reverse(12)); 
-        //obj.calc();
+        obj.input();
+        //System.out.println(obj.reverse(12)); 
+        obj.calc();
     }
 }
