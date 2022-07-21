b870b6b7c0afc2133ac637017742365228cc2b93
Labrec123 <103172844+shanuvs10@users.noreply.github.com>
2022-07-21 15:00:06 +0530

    Update date_generation.java

--- date_generation.java
+++ date_generation.java
@@ -3,7 +3,7 @@ import java.util.Scanner;
 public class date_generation {
 
   int dayno, year, dayadd, day;
-  int arr[] = { 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31 };
+  int arr[] = { 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31 };// array of months
   String name[] = {
     "JANUARY",
     "FEBRUARY",
@@ -19,7 +19,7 @@ public class date_generation {
     "DECEMBER",
   };
 
-  void input() {
+  void input() { //inputing all the values
     Scanner sc = new Scanner(System.in);
     System.out.println("Enter the Day no. ");
     dayno = sc.nextInt();
@@ -27,12 +27,21 @@ public class date_generation {
     year = sc.nextInt();
     System.out.println("Enter the Day to be added ");
     dayadd = sc.nextInt();
-    if(year%400==0 || (year%100!=0 && year%4==0)){
+    if(dayno<1 || dayno>366){
+    System.out.println("INVALID DAY NUMBER.");
+    System.exit(1);}
+    else if(year<1000 || year >9999){
+    System.out.println("INVALID YEAR");
+    System.exit(1);}
+    else if(dayadd<1 || dayadd>100){
+    System.out.println("INVALID DATE AFTER VALUE.");
+    System.exit(1);}
+    if(year%400==0 || (year%100!=0 && year%4==0)){//check for leap year
      arr[1] = 29;
     }
   }
 
-  int findmonth(int n) {
+  int findmonth(int n) { //finding no of months
     int i=0,month=0;
     day=dayno;
         while(day>arr[i])
@@ -44,14 +53,14 @@ public class date_generation {
     return month;
   }
 
-  void calc() {
+  void calc() { //calculation for the day
     display(findmonth(dayno));
     System.out.println("Days after "+dayadd);
     int nexday=dayno+dayadd;
     display(findmonth(nexday));
   }
 
-  void display(int p){
+  void display(int p){ // printing the day
     String suf;
     if(day%10==1 && day/10!=1)
             suf="ST";
@@ -65,7 +74,7 @@ public class date_generation {
         name[p]+" "+year);
   }
 
-  public static void main(String[] args) {
+  public static void main(String[] args) { // main method
     date_generation obj = new date_generation();
     obj.input();
     obj.calc();
