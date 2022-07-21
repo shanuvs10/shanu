d5a6cf972a16e9d48e23b6575dd49e2f4b5561b2
Labrec123 <103172844+shanuvs10@users.noreply.github.com>
2022-07-21 14:40:32 +0530

    Update date_generation.java

--- date_generation.java
+++ date_generation.java
@@ -2,7 +2,7 @@ import java.util.Scanner;
 
 public class date_generation {
 
-  int dayno, year, dayadd;
+  int dayno, year, dayadd, day;
   int arr[] = { 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31 };
   String name[] = {
     "JANUARY",
@@ -27,80 +27,47 @@ public class date_generation {
     year = sc.nextInt();
     System.out.println("Enter the Day to be added ");
     dayadd = sc.nextInt();
-    if(year % 4 == 0)
+    if(year%400==0 || (year%100!=0 && year%4==0)){
      arr[1] = 29;
+    }
   }
 
-  void yearcount(int n) {
-    int i = 0;
-    int a = 0, tem = 0;
-    for (; i >= arr.length; i++) {
-      tem = tem + arr[i];
-      a = tem - a;
-    }
-    return;
+  int findmonth(int n) {
+    int i=0,month=0;
+    day=dayno;
+        while(day>arr[i])
+        {
+            day-=arr[i];
+            i++;
+        }
+        month=i;
+    return month;
   }
 
   void calc() {
-    //IF IT IS A LEAP YEAR FEBRURAY SHOULD HAVE 29 DAYS
-    if(year%400==0 || (year%100!=0 && year%4==0))
-        month_days[1]=29;
+    display(findmonth(dayno));
+    System.out.println("Days after "+dayadd);
+    int nexday=dayno+dayadd;
+    display(findmonth(nexday));
+  }
 
-        i=0;
-   //FIND THE DATE CORRESPONDING TO THE DAY NUMBER
-        day=day_number;
-        while(day>month_days[i])
-        {
-            day-=month_days[i];
-            i++;
-        }
-        month=i;
-        //ADD SUFFIX AS PER THE DAY
-        if(day%10==1 && day/10!=1)
-            suffix="ST";
+  void display(int p){
+    String suf;
+    if(day%10==1 && day/10!=1)
+            suf="ST";
         else if(day%10==2 && day/10!=1)
-            suffix="ND";
+            suf="ND";
         else if(day%10==3 && day/10!=1)
-            suffix="RD";
+            suf="RD";
         else
-            suffix="TH";
-        System.out.println("OUTPUT:");
-        //FIRST PART OF THE OUTPUT
-        System.out.println(day+suffix+" "+
-        month_names[month]+" "+year);
-
-        //TO CALCULATE DATE AFTER N DAYS
-        day_after=day_number+n;
-        i=0;
-        while(day_after>month_days[i])
-        {
-            day_after-=month_days[i];
-            i++;
-            if(i==12){
-            i=0;
-            year++;
-            if(year%400==0 || (year%100!=0 && year%4==0))
-                month_days[1]=29;
-            }
-        }
-        day=day_after;
-        month=i;
-        //ADD SUFFIX AS PER THE DAY
-        if(day%10==1 && day/10!=1)
-            suffix="ST";
-        else if(day%10==2 && day/10!=1)
-            suffix="ND";
-        else if(day%10==3 && day/10!=1)
-            suffix="RD";
-        else
-            suffix="TH";
-       //SECOND PART OF THE OUTPUT
-        System.out.println(day+suffix+" "+
-        month_names[month]+" "+year);
+            suf="TH";
+        System.out.println(day+suf+" "+
+        name[p]+" "+year);
   }
 
   public static void main(String[] args) {
     date_generation obj = new date_generation();
     obj.input();
+    obj.calc();
   }
 }
