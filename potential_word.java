bdf86ebd1e1bf3e81d7beb6611be2384fa298eee
Labrec123 <103172844+shanuvs10@users.noreply.github.com>
2022-12-01 14:18:22 +0530

    Update potential_word.java

--- potential_word.java
+++ potential_word.java
@@ -1,3 +1,51 @@
+
 public class potential_word {
+    String strin;int a[];
+    String b[];
+    static String s;
+    void input(){
+        int c=0;
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter the String: ");
+        strin=sc.nextLine();   
+        strin=strin.toUpperCase();
+        strin=strin.trim();
+        strin=strin+" ";
+        s=strin;
+        for (int i = 0; i < s.length(); i++) {
+            if(s.charAt(i)==' '){
+                c++;
+                }
+                
+            }System.out.println(c);
+    }
+    int pot(String s){
+        int num=0;
+        for (int i = 0; i < s.length(); i++) {
+            num=num+(int)s.chartAt(i);
+        }
+        return num;
+    }
+    void calc(){
+        String s=strin,tem;
+        s=s.substring(0,s.length()-1);
+        int k=0,c=0;
+        for (int i = 0; i < s.length(); i++) {
+            if(s.charAt(i)==' '){
+                tem=s.substring(k,i); 
+                tem=tem.trim();
+                pot(s);
+                }
+                
+            }
+        
+        
+        System.out.println("");
+        System.out.println("No of words beginning and ending with a vowel : "+c);
+    }
     
+    public static void main(String[] args) {
+        potential_word obj=new potential_word();
+        obj.input();
+    }
 }
