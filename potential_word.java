26af444f5a8e8c8c2e7e08cef3d250cfb5c1090e
shanuvs10 <103172844+shanuvs10@users.noreply.github.com>
2022-12-12 23:40:32 +0530

    di

--- potential_word.java
+++ potential_word.java
@@ -1,7 +1,7 @@
 import java.util.*;
 import java.io.*;
 public class potential_word {
-    String strin;int b[];
+    String strin;
     String a[];
     static String s;
     void input(){
@@ -11,9 +11,15 @@ public class potential_word {
         strin=sc.nextLine();   
         strin=strin.toUpperCase();
         strin=strin.trim();
-        strin=strin+" ";
+        strin=" "+strin+" ";
         s=strin;
+        for (int i = 0; i < s.length(); i++) {
+            if (s.charAt(i)=='.'||s.charAt(i)=='?'||s.charAt(i)=='!') {
+                strin=strin.substring(0, i);
+            }
+        }
         a = strin.split(" ");
+
     }
     int pot(String s){
         int num=0;
@@ -23,29 +29,51 @@ public class potential_word {
         return num;
     }
     void calc(){
-        for (int i = 0; i < a.length - 1; i++)
-            for (int j = 0; j < a.length - i - 1; j++)
+        for (int i = 1; i < a.length; i++) {
+            System.out.println(a[i]+" = "+pot(a[i]));
+        }
+        for (int i = 0; i < a.length - 1; i++){
+            for (int j = 0; j < a.length - i - 1; j++){
                 if (pot(a[j]) > pot(a[j + 1])) {
-                    so
                     String temp = a[j];
                     a[j] = a[j + 1];
                     a[j + 1] = temp;
                 }
             }
-    }
+            }
+        }
+        
+    
     void display(){
         for (int i = 1; i < a.length; i++) {
-            System.out.println(b[i]);
+            System.out.print(a[i]+" ");
         }
+        System.out.println("");
     }
   
 
     
     public static void main(String[] args) {
         potential_word obj=new potential_word();
-        //obj.input();
-        //obj.calc();
-        //obj.display();
-        System.out.println(obj.pot("h"));
+        int flag=0;
+        obj.input();
+        
+        for (int i = 0; i < s.length(); i++) {
+            if (s.charAt(i)!='.'||s.charAt(i)!='?'||s.charAt(i)!='!'||s.charAt(i)!=' '||Character.isDigit(s.charAt(i))) {
+                flag=0;
+            }
+            else{
+                flag=1;
+            }
+            
+        }
+        if(flag==1){
+           obj.calc(); 
+           obj.display();
+        }
+        else{
+            System.out.println("Invalid Statement");
+            System.exit(0);
+        }
     }
 }
