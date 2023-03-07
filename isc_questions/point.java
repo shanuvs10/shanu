3a6bb71f05d617883cfaf68bcf7710cdf29df6c4
shanuvs10 <103172844+shanuvs10@users.noreply.github.com>
2023-03-07 22:59:28 +0530

    new

--- isc_questions/point.java
+++ isc_questions/point.java
@@ -15,4 +15,23 @@ public class point {
         y=sc.nextDouble();
 
     }
+    point midpoint(point a,point b){
+        a.x=(a.x+b.x)/2;
+        a.y=(a.y+b.y)/2;
+        return b;
+    }
+    void displaypoint(){
+        System.out.println("X = "+x);
+        System.out.println("Y = "+y);
+    }
+    public static void main(){
+        point obj1=new point();
+        obj1.readpoint();
+        point obj2=new point();
+        obj2.readpoint();
+        point obj3=new point();
+        obj3.midpoint(obj1,obj2);
+        obj3.displaypoint();
+
+    }
 }
